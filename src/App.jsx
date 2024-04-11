f240aac97de5b5ff001238727cd45ca7cc37257f
Harvinder Singh <hs541156@gmail.com>
2024-04-11 22:20:06 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function App() {
+  return (
+    <div>
+      <h1 className="text-center text-green-600">Interview with Inkoop </h1>
+    </div>
+  );
+}
+
+export default App;
